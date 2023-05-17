c1eb73874aa8c2f209a7841c53fcdd2f746b402a
fogolar1 <ENZO.KF@pch.local>
2023-05-17 16:54:25 -0300

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <Main />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import Main from "@/components/Main.vue";
+</script>
+
+<style>
+@font-face {
+  font-family: "Mogra";
+  src: local("Mogra"), url(./fonts/Mogra/Mogra-Regular.ttf) format("truetype");
+}
+</style>
