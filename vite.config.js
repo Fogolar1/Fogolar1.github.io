e4bcca3de481585d5e2d66acaee7031634019662
Fogolar1 <enzkf@outlook.com>
2025-05-31 22:59:49 -0300

    add base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/fogolar1.github.io/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
